a4f36d0e93a6d3a22028754632a385f37bec955a
William Santosa <93417386+williamsantosa@users.noreply.github.com>
2022-12-16 18:31:09 -0800

    updated

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018-2022 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without
+ * the express written permission of the copyright holder.
+ */
+
+import React from 'react';
+// import {useState, useEffect, useCallback} from 'react';
+import {
+  Typography,
+  // Typography, AppBar, Toolbar, CssBaseline,
+  // Drawer, Box, Divider,
+  // IconButton, Button,
+  // ListItem, ListItemButton, ListItemIcon, ListItemText,
+  // Table, TableBody, TableCell, TableContainer,
+  // TableHead, TableRow, Paper,
+} from '@mui/material/';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import InboxIcon from '@mui/icons-material/Inbox';
+// import MailIcon from '@mui/icons-material/Mail';
+// import ClearIcon from '@mui/icons-material/Clear';
+
+/**
+ * Returns app component
+ * @return {object}
+ */
+function App() {
+  return (
+    <>
+      <Typography>Hello</Typography>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2022 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without
+ * the express written permission of the copyright holder.
+ */
+
+/** ****************  DO NOT MODIFY THIS FILE ****************************
+ *
+ * It is not included in the subission archive ceated by 'npm run zip' .
+ *
+ ************************************************************************/
+
+import {createRoot} from 'react-dom/client';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(<App/>);
